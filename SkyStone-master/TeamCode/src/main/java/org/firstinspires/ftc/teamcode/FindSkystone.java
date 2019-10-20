6bbde349e4a6e388d25bfb9698e6478e91125977
noahisthebestpersonever <49132668+noahisthebestpersonever@users.noreply.github.com>
2019-10-20 11:55:58 -0700

    Worked on Findiing Skystones
    Renamed file and class and added angle to skystone, and image to object height ratio to detect distance

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FindSkystone.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FindSkystone.java
@@ -32,9 +32,13 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
@@ -49,9 +53,9 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
-@TeleOp(name = "Skystone: TensorFlow Object Detection Webcam", group = "Concept")
+@TeleOp(name = "Find Skystone")
 //@Disabled
-public class SkystoneTensorFlowObjectDetectionWebcam extends LinearOpMode {
+public class FindSkystone extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Stone";
     private static final String LABEL_SECOND_ELEMENT = "Skystone";
@@ -118,13 +122,46 @@ public class SkystoneTensorFlowObjectDetectionWebcam extends LinearOpMode {
                         telemetry.addData("# Object Detected", updatedRecognitions.size());
                         // step through the list of recognitions and display boundary info.
                         int i = 0;
+                        List<Recognition> skystone = new ArrayList<Recognition>();
+                        List<Recognition> stone = new ArrayList<Recognition>();
                         for (Recognition recognition : updatedRecognitions) {
                             telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
                             telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
                                     recognition.getLeft(), recognition.getTop());
                             telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
                                     recognition.getRight(), recognition.getBottom());
+
+                            float objectHeight = recognition.getHeight();
+                            float imageHeight = recognition.getImageHeight();
+                            float ratioHeight = imageHeight/objectHeight;
+                            double horizontalAngle = recognition.estimateAngleToObject(AngleUnit.DEGREES);
+
+                            telemetry.addData("Estimated Angle", "%.3f", horizontalAngle);
+                            telemetry.addData("Height Ratio", "%.3f", ratioHeight);
+                            telemetry.addData("Object Height", "%.3f", objectHeight);
+                            telemetry.addData("Image Height", "%.3f", imageHeight);
+                            if (recognition.getLabel().equals("Skystone")){
+                                //Add to Skystone List
+                                skystone.add(recognition);
+                            }else {
+                                //Add to Stone List
+                                stone.add(recognition);
+                            }
                         }
+
+                        //Check if Skystone list is empty
+                        if(skystone.size() > 0){
+                            //Recognizes Skystone
+                            telemetry.addData("Skystone Detected: ", skystone.size());
+                        }else if(stone.size() > 0){
+                            //No Skystones, Move to stone
+                            telemetry.addData("No Skystones Detected, Stones Detected: ", stone.size());
+                        }else{
+                            //Nothing Detected
+                            telemetry.addData("Nothing Detected", 0);
+                        }
+
+
                         telemetry.update();
                     }
                 }
@@ -165,4 +202,5 @@ public class SkystoneTensorFlowObjectDetectionWebcam extends LinearOpMode {
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
         tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
     }
+
 }
